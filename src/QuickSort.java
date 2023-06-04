39c9a762be5964f7ebb5a5d6d98a1eb56eb6240b
leorenis <leorenis@gmail.com>
2023-06-04 19:59:55 -0300

    fix: quick,mergesort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -3,34 +3,36 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int [] numbers = { 8, 9, 78, 1, 3, 5, 0, 6 };
+        int [] numbers = {  9, 6, 10, 44, 7, 1, 5, 2 };
         // int [] numbers = { 9, 6, 10, 44, 7, 1, 5, 2 }; // Essa deu treta, investigar
         quickSort(numbers, 0, numbers.length -1);
         System.out.println(Arrays.toString(numbers));
     }
 
-    private static void quickSort(int[] n, int left, int right){
+    private static void quickSort(int[] n, int begin_pos, int end_pos){
         int bkp;
-        var i = left;
-        var j = right;
-        var pivot = n[left];
+        var l = begin_pos;
+        var r = end_pos;
+        var pivot = n[begin_pos];
 
-        while (i <= j) {
+        while (l <= r) {
 
-            while (n[i] < pivot) i ++;
-            while (n[j] < pivot) j --; // Descrescente, pra tornar crescente, alterar o sinal de menor que pra maior que
+            while (n[l] < pivot) l ++;
+            while (n[r] > pivot) r --;
 
-            if (i <= j) {
-                bkp = n[i];
-                n[i] = n[j]; n[j] = bkp;
-                i++; j--;
+            if (l <= r) {
+                bkp = n[l];
+                n[l] = n[r];
+                n[r] = bkp;
+                l++;
+                r--;
             }
         }
 
-        if (left < j)
-            quickSort(n, left, j);
+        if (begin_pos < r)
+            quickSort(n, begin_pos, r);
 
-        if (i < right)
-            quickSort(n, i, right);
+        if (l < end_pos)
+            quickSort(n, l, end_pos);
     }
 }
