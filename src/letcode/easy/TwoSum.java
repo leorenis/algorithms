3a26ee93f563f7251799239fc879288d361d14de
leorenis <leorenis@gmail.com>
2024-12-02 20:39:24 -0300

    TwoSum

--- src/letcode/easy/TwoSum.java
+++ src/letcode/easy/TwoSum.java
@@ -9,12 +9,12 @@ public class TwoSum {
         int[] elementos = {2,7,11,15};
         var dicionario = new HashMap<Integer, Integer>();
         while (pivo < elementos.length) {
-            dicionario.put(elementos[pivo], pivo);
             var elemento = dicionario.get(alvo - elementos[pivo]);
             if (elemento != null && pivo != elemento) {
                 System.out.printf("Alvos: %d e %d", elemento, pivo);
                 break;
             }
+            dicionario.put(elementos[pivo], pivo);
             pivo++;
         }
     }
